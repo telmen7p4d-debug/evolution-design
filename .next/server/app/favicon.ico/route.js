e8825c19bfe0b81d0fdc2e29efc1cf6358c40f61
telmen7p4d-debug <telmen7p4d@moes.edu.mn>
2026-05-08 18:47:59 +0800

    Add .next/server/app/favicon.ico/route.js

---
+++ .next/server/app/favicon.ico/route.js
@@ -0,0 +1,6 @@
+var R=require("../../chunks/[turbopack]_runtime.js")("server/app/favicon.ico/route.js")
+R.c("server/chunks/[externals]_next_dist_0sqmaqd._.js")
+R.c("server/chunks/[root-of-the-server]__0u7n-la._.js")
+R.c("server/chunks/output_evolution__next-internal_server_app_favicon_ico_route_actions_0wznsr6.js")
+R.m(85289)
+module.exports=R.m(85289).exports
